e5b0f4ad9a6b2b373ba543fe5b6763d10854919b
Dmitry Moiseev <m0sia@m0sia.ru>
2013-11-18 16:15:20 +0600

    Added stdeb support

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
 import glob
-import py2exe
+import sys
+
+if sys.platform=="win32":
+  import py2exe
+  from distutils.core import setup
+
 
 setup(
     name = 'parrotziktray',
-    description = 'Parrot Zik Tray',
-    version = '0.1',
+    description = 'Parrot Zik Tray Indicator',
+    author="Dmitry Moiseev",
+    author_email="m0sia@m0sia.ru",
+    maintainer_email="m0sia@m0sia.ru",
+    url="https://github.com/m0sia/pyParrotZik",
+    license="'GPLv2+'",
+    version = '0.2',
 
     windows = [
                   {
-                      'script': 'ParrotZikTray.py',
-                      'icon_resources': [(1, "./icons/Headphone.ico")],
+                      'script': 'ParrotZikTray',
+                      'icon_resources': [(1, "./share/icons/zik/Headphone.ico")],
                   }
               ],
 
@@ -21,11 +31,19 @@ setup(
                       'includes': 'cairo, pango, pangocairo, atk, gobject, gio, gtk.keysyms, _winreg',
                   }
               },
-
+   
     data_files=[
-    				("icons", glob.glob("icons/*.png"))
+            ("share/icons/zik", glob.glob("share/icons/zik/*.png"))
                    # If using GTK+'s built in SVG support, uncomment these
                    #os.path.join(gtk_base_path, '..', 'runtime', 'bin', 'gdk-pixbuf-query-loaders.exe'),
                    #os.path.join(gtk_base_path, '..', 'runtime', 'bin', 'libxml2-2.dll'),
-               ]
+               ],
+
+    install_requires=[
+          'BeautifulSoup','bluetooth'
+      ],
+
+    py_modules=['ParrotZik','ParrotProtocol'],
+
+    scripts=["ParrotZikTray"]
 )
