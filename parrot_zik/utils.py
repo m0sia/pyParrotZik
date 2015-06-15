65f9c06e00c78c382451458ea1d8a9dad1016fb0
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2015-06-15 12:55:18 +0200

    Refactor. Move

---
+++ parrot_zik/utils.py
@@ -0,0 +1,31 @@
+from threading import Lock
+
+import gtk
+
+
+class repeat(object):
+    def __init__(self, f):
+        self.f = f
+        self.id = None
+        self.lock = Lock()
+
+    def __call__(self, cls):
+        self.f(cls)
+
+    def start(self, cls, frequency):
+        self.lock.acquire()
+        if not self.id:
+            def run():
+                self.f(cls)
+                return True
+
+            self.id = gtk.timeout_add(frequency, run)
+        self.lock.release()
+
+    def stop(self):
+        self.lock.acquire()
+        if self.id:
+            gtk.timeout_remove(self.id)
+            self.id = None
+        self.lock.release()
+
